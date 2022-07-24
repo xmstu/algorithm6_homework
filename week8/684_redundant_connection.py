399429280064afc5ac296a394778a10f0db6549a
hexm <hexm@huitouche.com>
2022-07-24 16:49:42 +0800

    ftr: 更新题解

--- week8/684_redundant_connection.py
+++ week8/684_redundant_connection.py
@@ -2,37 +2,58 @@
 from typing import List
 
 
+class DisjointSet:
+    """
+    并查集
+
+    find 将 x 和 x 的所有祖先连在根节点上, 因此树的高度只有2层
+    unionSet 把两棵树进行合并, 只有当两个节点的最高层级的祖先不一致时, 才合并
+    """
+
+    def __init__(self, n) -> None:
+        self.fa = [i for i in range(n)]
+    
+    def find(self, x):
+        if x == self.fa[x]:
+            return x
+        self.fa[x] = self.find(self.fa[x])
+        return self.fa[x]
+    
+    def unionSet(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x != y:
+            self.fa[x] = y
+
+    def connected(self, x, y):
+        if self.find(x) == self.find(y):
+            return True
+        else:
+            return False
+
+
 class Solution:
+
+    """
+    思路:
+        例子: edges = [[1,2], [1,3], [2,3]]
+        利用并查集, 在每次遍历的时候划分分组,
+        第一次遍历, edge: [1, 2], 1 归为 2 这个帮派
+        第二次遍历, edge: [1, 3], 1 的父亲是 2, 2 和 3 组成更大的帮派, 此时, 2 的 父亲变为 3
+        第三次遍历, edge: [2, 3], 2 的父亲是 3, 3 的父亲是自己, 已经连接了, 因此 [2, 3] 为冗余边
+    """
+    
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = 0
-        for edge in edges:
-            x, y = edge[0], edge[1]
-            n = max(n, max(x, y))
-        n = n + 1
-        # 出边数组
-        self.to = [[] for _ in range(n)]
-        self.visited = [False for _ in range(n)]
-        self.has_cycle = False
-        for edge in edges:
-            x, y = edge[0], edge[1]
-            self.to[x].append(y)
-            self.to[y].append(x)
-            # 每次dfs完要记得初始化 visted 数组
-            self.visited = [False for _ in range(n)]
-            self.dfs(x, 0)
-            if self.has_cycle:
-                return edge
-
-    def dfs(self, x: int, father: int):
-        self.visited[x] = True
-
-        for y in self.to[x]:
-            if y == father:
-                continue
-            if not self.visited[y]:
-                self.dfs(y, x)
+        n = len(edges)
+        disjointSet = DisjointSet(n+1)
+        for i in range(n):
+            # 提前判断是否相连, 
+            if disjointSet.connected(edges[i][0], edges[i][1]):
+                return edges[i]
             else:
-                self.has_cycle = True
+                disjointSet.unionSet(edges[i][0], edges[i][1])
+        
+        return []
 
 
 class TestFindRedundantConnection:
@@ -55,5 +76,3 @@ class TestFindRedundantConnection:
 
         edges = [[1,5],[3,4],[3,5],[4,5],[2,4]]
         assert [4,5] == solution.findRedundantConnection(edges)
-
-
